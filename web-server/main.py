cc6b45714d721e88cc81bc4289ddc4f46226a53d
Jesús Duque <jeduquel@gmail.com>
2024-05-20 15:44:13 -0500

    add fastAPI to web-server application

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,24 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+  return [1, 2, 3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+  #return {'name': 'Jesús'}
+  return """
+  <h1>Hola, soy una página web</h1>
+  <p>Párrafo</p>
+
+  """
+
 
 def run():
   store.get_categories()
